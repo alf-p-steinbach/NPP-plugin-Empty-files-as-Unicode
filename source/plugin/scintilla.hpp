467150bb224eedc58e26de584899b41444472ae1
Alf P. Steinbach <alf.p.steinbach@gmail.com>
2018-01-04 03:08:16 +0100

    New directory structore. Support for 64-bit build.

--- source/plugin/scintilla.hpp
+++ source/plugin/scintilla.hpp
@@ -6,10 +6,10 @@ namespace scintilla {
 
     inline auto codepage( const HWND handle )
         -> int
-    { return ::SendMessage( handle, SCI_GETCODEPAGE, 0, 0 ); }
+    { return static_cast<int>( ::SendMessage( handle, SCI_GETCODEPAGE, 0, 0 ) ); }
 
     inline auto length( const HWND handle )
         -> int
-    { return ::SendMessage( handle, SCI_GETLENGTH, 0, 0 ); }
+    { return static_cast<int>( ::SendMessage( handle, SCI_GETLENGTH, 0, 0 ) ); }
 
 }  // namespace scintilla
