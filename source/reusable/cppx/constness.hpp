cf754016ec9dc9bd0cafda399f5b6433af59f775
Alf P. Steinbach <alf.p.steinbach@gmail.com>
2018-01-07 09:59:26 +0100

    More clean constness adaption

--- source/reusable/cppx/constness.hpp
+++ source/reusable/cppx/constness.hpp
@@ -19,9 +19,30 @@ namespace cppx{
         };
     }  // namespace impl
 
+    // The enumeration type allows templating a type on constness without introducing
+    // an open ended number of possible instantiations, but just the two, m and c.
+    struct Constness{ enum Enum { m, c }; };        // m = mutable, c = const
+
+    template< Constness::Enum c > struct As_type_;
+
+    template<>
+    struct As_type_<Constness::m>
+    {
+        using T = void;
+    };
+
+    template<>
+    struct As_type_<Constness::c>
+    {
+        using T = const void;
+    };
+
     template< class Other, class Type >
     using const_like_ = typename impl::With_const_as_t_<
         remove_reference_t<Other>, Type
         >::T;
+
+    template< Constness::Enum c, class Type >
+    using with_constness_ = const_like_<typename As_type_<c>::T, Type>;
 
 }  // namespace cppx
--- source/reusable/npp/Config.hpp
+++ source/reusable/npp/Config.hpp
@@ -1,5 +1,5 @@
 #pragma once
-#include <cppx/const_like_.hpp>
+#include <cppx/constness.hpp>                   // cppx::const_like_
 #include <cppx/class-kind/No_copy.hpp>
 #include <npp/Encoding_config.hpp>              // npp::Encoding_config
 #include <npp/paths.hpp>                        // npp::path_to_gui_config
@@ -18,7 +18,9 @@ namespace npp {
     namespace tx2 = tinyxml2;
 
     using cppx::const_like_;
+    using cppx::Constness;
     using cppx::No_copy;
+    using cppx::with_constness_;
     using std::stoi;
     using std::string;
     using std::unique_ptr;
@@ -26,13 +28,12 @@ namespace npp {
 
     using Path = fs::path;
 
-    class Config
-        : public No_copy
-    {
-        unique_ptr<tx2::XMLDocument>    p_xml_;     // Dynamic in order to be non-const.
+    class Config;
+
+    namespace config_impl {
 
         template< class Type >
-        static auto non_null( ref_<const string> msg, const ptr_<Type> p )
+        auto non_null( ref_<const string> msg, const ptr_<Type> p )
             -> ptr_<Type>
         {
             hopefully( p != nullptr )
@@ -40,42 +41,60 @@ namespace npp {
             return p;
         }
 
-        template< class Self >
-        static auto root( ref_<Self> self )
-            -> ptr_<const_like_<Self, tx2::XMLElement>>
+        template< Constness::Enum constness >       // m or c, for mutable or const
+        class Const_adaptation_
         {
-            return non_null( "root - no xml root element", self.p_xml_->RootElement() );
-        }
+        protected:
+            using Self = with_constness_<constness, Config>;
 
-        template< class Self >
-        static auto gui_configs( ref_<Self> self )
-            -> ptr_<const_like_<Self, tx2::XMLElement>>
-        {
-            return non_null(
-                "gui_configs - no xml GUIConfigs element",
-                root( self )->FirstChildElement( "GUIConfigs" )
-                );
-        }
-
-        template< class Self >
-        static auto config_of_new_doc( ref_<Self> self )
-            -> ptr_<const_like_<Self, tx2::XMLElement>>
-        {
-            auto p_config = gui_configs( self )->FirstChildElement();
-            while( p_config != nullptr )
+            static auto root( ref_<Self> self )
+                -> ptr_<const_like_<Self, tx2::XMLElement>>
             {
-                const ptr_<const char> p_name = p_config->Attribute( "name" );
-                if( p_name != nullptr and p_name == "NewDocDefaultSettings"s )
-                {
-                    break;
-                }
-                p_config = p_config->NextSiblingElement();
+                return non_null( "root - no xml root element", self.p_xml_->RootElement() );
             }
-            return non_null(
-                "config_of_new_doc - no xml element named NewDocDefaultSettings",
-                p_config
-                );
-        }
+
+            static auto gui_configs( ref_<Self> self )
+                -> ptr_<const_like_<Self, tx2::XMLElement>>
+            {
+                return non_null(
+                    "gui_configs - no xml GUIConfigs element",
+                    root( self )->FirstChildElement( "GUIConfigs" )
+                    );
+            }
+
+            static auto config_of_new_doc( ref_<Self> self )
+                -> ptr_<const_like_<Self, tx2::XMLElement>>
+            {
+                auto p_config = gui_configs( self )->FirstChildElement();
+                while( p_config != nullptr )
+                {
+                    const ptr_<const char> p_name = p_config->Attribute( "name" );
+                    if( p_name != nullptr and p_name == "NewDocDefaultSettings"s )
+                    {
+                        break;
+                    }
+                    p_config = p_config->NextSiblingElement();
+                }
+                return non_null(
+                    "config_of_new_doc - no xml element named NewDocDefaultSettings",
+                    p_config
+                    );
+            }
+        };
+    }  // namespace config_impl
+
+    class Config
+        : public No_copy
+        , private config_impl::Const_adaptation_<Constness::m>
+        , private config_impl::Const_adaptation_<Constness::c>
+    {
+        unique_ptr<tx2::XMLDocument>    p_xml_;     // Dynamic in order to be non-const.
+
+        using Consted = config_impl::Const_adaptation_<Constness::c>;  friend Consted;
+        using Mutable = config_impl::Const_adaptation_<Constness::m>;  friend Mutable;
+
+        using Consted::config_of_new_doc;
+        using Mutable::config_of_new_doc;
 
     public:
         auto default_encoding() const
@@ -105,7 +124,6 @@ namespace npp {
             const string ansi_config_path = char_path_or_x( path );
             p_xml_->LoadFile( ansi_config_path.c_str() ) >> Success{}
                 or fail( "npp::Config::<init> - tinyxml2::XMLDocument::LoadFile failed" );
-
             //hopefully( xml_.RootElement()->Name() == "NotepadPlus"s )
             //    or fail( "npp::Config::<init> -  XML root element is not “NotepadPlus”" );
         }
